69f1a5ac7164ec8dcf8cba0da58e53816f1edbd8
802023 <136094955+devwebsoluctions@users.noreply.github.com>
2024-03-13 22:42:17 -0300

    Adicionando Steam

--- src/steams/task-csv.js
+++ src/steams/task-csv.js
@@ -2,4 +2,39 @@ import {parse} from 'csv-parse';
 
 import fs from 'node:fs';
 
+let numeroAleatorio = Math.floor(Math.random() * 100);
 
+const csvPath = new URL(`./arquivo-csv/arquivo-${numeroAleatorio}.csv`, import.meta.url);
+
+const stream = fs.createReadStream(csvPath);
+
+const csvStream = parse({
+    delimiter: ',',
+    skipEmptyLines: true,
+    fromLine: 2
+})
+
+async function run(){
+    const lineParse = stream.pipe(csvStream);
+
+    for await (const line of lineParse){
+        const [title, description] = line;
+
+        return JSON.stringify({
+            title,
+            description,
+        })    
+    }
+}
+
+fetch('http://localhost:3334/task', {
+    method: 'POST',
+    body: run(),
+    headers: {
+        'Content-Type': 'application/json',
+    },
+}).then(response => {
+    response.text().then(data => {
+        console.log(data)
+    })
+})
